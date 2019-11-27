e674bbc4da2b591b44bb719ac5e3c4ae57630983
brianbeutler <49566508+brianbeutler@users.noreply.github.com>
2019-11-27 15:55:42 -0700

    Update testGUI_brian.py

--- testGUI_brian.py
+++ testGUI_brian.py
@@ -9,7 +9,6 @@ from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person, SnapshotObjectType, OperationStatusType
-
  
 global KEY
 # Set the FACE_SUBSCRIPTION_KEY environment variable with your key as the value.
@@ -51,6 +50,12 @@ class MyVideoCapture:
             self.vid.release()
         self.window.mainloop()
 
+    def frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        else:
+            return (ret, None)
     def get_frame(self):
         if self.vid.isOpened():
             ret, frame = self.vid.read()
@@ -136,6 +141,140 @@ class MyVideoCapture:
                 return (ret, None)
         else:
             return (ret, None)
+from tkinter import *
+
+class createNew:
+    def __init__(self):
+        self._root = Tk()
+        self.vid = MyVideoCapture(video_source=0)
+        #root = Tk() ?
+
+        #set size of window
+        self._root.geometry("500x500") 
+        
+        #set the user input elements
+        self._name = Label(self._root, text="Name: ")
+        #password = Label(self._root, text="Password: ")
+        #cfmPwd = Label(self._root, text="Confirm Password: ")
+        entry_1 = Entry(self._root)
+        #entry_2 = Entry(self._root)
+        #entry_3 = Entry(self._root)
+        self._name.grid(row=0, sticky=E) #sticky E = east, right aligned
+        #password.grid(row=1, sticky=E)
+        #cfmPwd.grid(row=2, sticky=E)
+        entry_1.grid(row=0, column=1)
+        #entry_2.grid(row=1, column=1)
+        #entry_3.grid(row=2, column=1)
+     
+        #submit button
+        b = Button(self._root, text="Submit",fg="white", bg="black", command=self.addUser)
+
+        b.grid(columnspan=2)
+
+        self._root.mainloop()
+
+    def addUser(self):
+            
+        while True:
+            # Capture frame-by-frame
+            ret, frame = self.vid.frame()
+            #frame = cv2.resize(frame,(1280,720),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+            #cv2.imshow('Video', frame)
+        
+        
+            file_name = path + Name + str(i) + '.jpg'
+
+            print ('Creating...' + file_name)
+            cv2.imwrite(file_name, frame)
+            
+            IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+            
+            image_array = glob.glob(os.path.join(IMAGES_FOLDER, file_name))
+            if image_array:
+                image = open(image_array[0], 'r+b')
+
+                # Detect faces
+                faces = face_client.face.detect_with_stream(image, return_face_id=True, return_face_landmarks=True, return_face_attributes=['age', 'gender', 'headPose', 'smile', 'facialHair', 'glasses', 'emotion'], recognition_model='recognition_02', return_recognition_model=False, detection_model='detection_01', custom_headers=None, raw=False, callback=None)
+                if faces != []:
+                    rectanle = faces[0].face_rectangle
+
+                    if ((rectanle.height > 200) and (rectanle.width> 200)):
+                        #only keep picture if race rectangle is bigger than 200 x 200 (azure reccomends)
+
+                        i+=1
+                    else:
+                        #remove image 
+                        image.close()
+                        os.remove(file_name)
+                else:
+                    #remove image 
+                    image.close()
+                    os.remove(file_name)
+            
+                if i == 5:
+                    #takes 50 images
+                    break
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    video_capture.release()
+                    cv2.destroyAllWindows()
+                    sys.exit()
+        video_capture.release()
+        cv2.destroyAllWindows()
+    
+        new_person = face_client.person_group_person.create(PERSON_GROUP_ID, name= Name, user_data=None, recognition_model='recognition_02', custom_headers=None, raw=False)
+        new_person.name = Name
+
+        '''
+        Detect faces and register to correct person
+        '''
+        # Find all jpeg images in working directory
+    
+
+        # Adds images to person in persongroup
+        IMAGES_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    
+    
+        for i in range(1,5):
+            file_name = path + Name + str(i) + '.jpg'
+            image_array = glob.glob(os.path.join(IMAGES_FOLDER, file_name))
+            print('Adding: ',image_array[0])
+            image = open(image_array[0], 'r+b')
+            face_client.person_group_person.add_face_from_stream(PERSON_GROUP_ID, new_person.person_id, image)
+            image.close()
+        print()
+   
+        for i in range(1,5):
+            file_name = path + Name + str(i) + '.jpg'
+            image_array = glob.glob(os.path.join(IMAGES_FOLDER, file_name))
+
+            try:
+                print('Removing: ',image_array[0])
+                os.remove(file_name)
+            except:
+                print('Didnt remove: ',file_name)
+
+    
+    
+
+        ''' 
+        Train PersonGroup
+        '''
+        print()
+        print('Training the person group...')
+        # Train the person group
+        face_client.person_group.train(PERSON_GROUP_ID)
+
+        while (True):
+            training_status = face_client.person_group.get_training_status(PERSON_GROUP_ID)
+            print("Training status: {}.".format(training_status.status))
+            print()
+            if (training_status.status is TrainingStatusType.succeeded):
+                break
+            elif (training_status.status is TrainingStatusType.failed):
+                sys.exit('Training the person group has failed.')
+
+        
 class App:
     def __init__(self, window, window_title, video_source=0):
         self.window = window
@@ -149,14 +288,33 @@ class App:
         self.canvas = Canvas(self.window, width = self.vid.width, height = self.vid.height)
         self.canvas.pack()
             
+        #add buttons
+        f = Frame(window)
+        f.pack()
+    
+        #Containers
+        leftFrame = Frame(f)
+        leftFrame.pack(side=LEFT)
+        bottomFrame = Frame(f)
+        bottomFrame.pack(side=BOTTOM)
+        rightFrame = Frame(f)
+        rightFrame.pack(side=RIGHT)
 
-        #snapshot button
-        self.btn_snapshot= Button(window, text="Add New User", width=50, command=self.snapshot, bg="black", fg="white")
-        self.btn_snapshot.pack(anchor=CENTER, expand=True)
-        self.snapshot()
+        #Direct to the add new user page
+        add = Button(rightFrame, text="Add User Button", fg="white", bg="black", command=self.directNew)
+        add.pack()
+        add.grid(padx=20, pady=20)
+
+        #TODO1: For now, it's after pressing the button, the window shows up
+        #TODO2: Create status bar for login status
+        #show = Button(leftFrame, text="Recognize faces", fg="white", bg="Red")
+        #show.pack()
+        #show.grid(padx=50, pady=50)
+     
         self.delay = 5
         self.update()
-        
+        root.bind('<Escape>', lambda e: root.destroy())
+
         self.window.mainloop()
     
     def update(self):
@@ -167,14 +325,18 @@ class App:
             self.canvas.create_image(0, 0, image = self.photo, anchor = NW)
         self.window.after(self.delay, self.update)
 
-    def snapshot(self):
-         #Get a frame from the video source
-         ret, frame = self.vid.get_frame()
-         print("capture")
-         if ret:
-            cv2.imwrite(path + "frame" + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
             
-        
+    def directNew(self):
+    
+        self.window.destroy()
+        del self.vid
+        #f.destroy()
+        #f2 = Frame(root)
+        obj = createNew()
+        f2.pack()
+        return
+    
 # Create a window and pass it to the Application object
 root = Tk()
-App(root, "Tkinter and OpenCV")
+root.geometry("800x800+700+300")
+App(root, "Facial Recognition")
